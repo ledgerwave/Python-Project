1faa791aeef3e98f7c5f9f1b5f658dbf15e52a60
mirai <mirai.lucky.dev@gmail.com>
2024-09-28 10:18:09 +0500

    Improved the code

--- Automation/Interacting-with-os/files.py
+++ Automation/Interacting-with-os/files.py
@@ -5,21 +5,20 @@ Working with files using the `OS` library
 import os
 import datetime
 
-file_size = os.path.getsize("checking_system.py")
+FILE_NAME = "checking_system.py"
+
+file_size = os.path.getsize(FILE_NAME)
 
 print(f"Size: {file_size} bytes")
 
-
-# Returns time in unix
-timestamp = os.path.getmtime("checking_system.py")
+# Last modified time, returns time in unix
+timestamp = os.path.getmtime(FILE_NAME)
 
 FORMATTED_DATETIME = datetime.datetime.fromtimestamp(timestamp)
 
 print(f"Last modified: {FORMATTED_DATETIME}")
 
-
 # Get Absolute path of the file
-
-abspath = os.path.abspath("checking_system.py")
+abspath = os.path.abspath(FILE_NAME)
 
 print(f"File path: {abspath}")
