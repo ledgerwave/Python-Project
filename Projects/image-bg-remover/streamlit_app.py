4815275a02bc594432df158ec2664c5222ac3348
mirai <mirai.lucky.dev@gmail.com>
2024-10-09 19:13:30 +0500

    Refactor image background remover streamlit app

--- Projects/image-bg-remover/streamlit_app.py
+++ Projects/image-bg-remover/streamlit_app.py
@@ -2,28 +2,56 @@
 Image background remover streamlit app
 """
 
+from io import BytesIO
 from rembg import remove
 from PIL import Image
 import streamlit as st
 
+st.title("Image Background Remover")
 
-def image_bg_remover(image_path: str):
+
+def image_bg_remover(image):
     """
     Removes background of the given image
     """
-    if image_path:
-        input_image = Image.open(image_path)
+    if image:
+        input_image = Image.open(image)
         output = remove(input_image)
-        st.write(output)
+        return output
+    return None
 
 
 def main():
     """
     Main function
     """
-    st.header("Image background remover")
-    image_path = st.camera_input(label="Insert picture")
-    image_bg_remover(image_path)
+    image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if image_file:
+        st.image(image_file, caption="Original Image", use_column_width=True)
+
+        # Process the image to remove the background
+        output_image = image_bg_remover(image_file)
+
+        if output_image:
+            st.image(
+                output_image,
+                caption="Image with Background Removed",
+                use_column_width=True,
+            )
+
+            # Convert the image to a BytesIO object to enable downloading
+            img_bytes = BytesIO()
+            output_image.save(img_bytes, format="PNG")
+            img_bytes.seek(0)
+
+            # Add a download button
+            st.download_button(
+                label="Download Image with Background Removed",
+                data=img_bytes,
+                file_name="background_removed.png",
+                mime="image/png",
+            )
 
 
 if __name__ == "__main__":
