fb74b984374286f06926a120cb15592a151292a5
mirai <mirai.lucky.dev@gmail.com>
2024-10-14 11:10:57 +0500

    Converted the program to OOP based

--- Programs/contact_book.py
+++ Programs/contact_book.py
@@ -2,62 +2,116 @@
 Contact Book program
 """
 
-TypeContact = dict[str, dict[str, str]]
+TypeContact = dict[str, str]
 
-contacts: TypeContact = {}
+TypeContacts = dict[str, TypeContact]
 
-while True:
-    print("1. Create Contact")
-    print("2. Update Contact")
-    print("3. Search Contact")
-    print("4. Delete Contact")
-    print("5. View Contacts")
-    print("6. Count Contacts")
-    print("7. Exit")
 
-    choice = input("> ")
+class ContactBook:
+    """
+    Contact book class
+    """
 
-    if choice == "1":
-        name = input("Enter name: ")
-        email = input("Enter email: ")
-        mobile = input("Enter phone number: ")
-        contacts[name] = {"name": name, "email": email, "mobile": mobile}
-        print("Contact added successfully")
-    elif choice == "2":
-        name = input("Enter name to update contact: ")
-        if name in contacts:
-            email = input("Enter email: ")
-            mobile_num = input("Enter phone number: ")
-            contacts[name].update({"email": email, "mobile": mobile})
+    def __init__(self):
+        self.__contacts = {}
+
+    def __str__(self):
+        return f"Contact book: Total contacts {len(self.__contacts)}"
+
+    def create_contact(self, contact: TypeContact) -> None:
+        """
+        Creates a new contact
+
+        Args:
+            contact (dict) : New contact dictionary
+        """
+        name, email, phone = contact["name"], contact["email"], contact["phone"]
+        if name in self.__contacts:
+            print("Contact name already exists!")
+        else:
+            self.__contacts[name] = {"name": name, "email": email, "phone": phone}
+            print("Contact added successfully")
+
+    def view_contacts(self) -> None:
+        """
+        View contacts
+        """
+        if self.__contacts:
+            print(f"Contacts: {self.__contacts}")
+        else:
+            print("Empty contact list!")
+
+    def update_contact(self, name: str, contact: TypeContact) -> None:
+        """
+        Updates the contact with the given name
+        """
+        if name in self.__contacts:
+            self.__contacts[name].update(contact)
             print("Contact updated successfully")
+        else:
+            print("Contact not found!")
 
+    def search_contact(self, name: str) -> TypeContact | None:
+        """
+        Searchs for a contact in list by given name
+        """
+        if name in self.__contacts:
+            return self.__contacts[name]
+        print("Contact not found!")
+        return None
+
+    def delete_contact(self, name: str) -> None:
+        """
+        Deletes a contact in list by given name
+        """
+        if name in self.__contacts:
+            del self.__contacts[name]
         else:
             print("Contact not found!")
-    elif choice == "3":
-        name = input("Enter name to search: ")
-        if name in contacts:
-            print(contacts[name])
-        else:
-            print("Contact not found!")
-    elif choice == "4":
-        name = input("Enter contact name to delete: ")
-        if name in contacts:
-            del contacts[name]
-            print("Contact deleted successfully!")
-        else:
-            print("Contact not found!")
-    elif choice == "5":
-        if contacts:
-            print(contacts)
-        else:
-            print("No contacts yet!")
-    elif choice == "6":
-        if contacts:
-            print(f"No. of contacts: {len(contacts)}")
-        else:
-            print("No contacts yet!")
-    elif choice == "7":
-        print("Contact book closed")
-        break
-    else:
-        print("Invalid command!")
+
+
+def main():
+    """
+    Main function
+    """
+
+    contact_book = ContactBook()
+
+    while True:
+        print("1. Create Contact")
+        print("2. Update Contact")
+        print("3. Search Contact")
+        print("4. Delete Contact")
+        print("5. View Contacts")
+        print("6. Exit")
+
+        command = input("> ")
+
+        if command == "1":
+            name = input("Enter name: ")
+            email = input("Enter email: ")
+            phone = input("Enter phone: ")
+            contact_book.create_contact({"name": name, "email": email, "phone": phone})
+        elif command == "2":
+            name = input("Enter name to update contact: ")
+            email = input("Enter email: ")
+            phone = input("Enter phone: ")
+            contact_book.update_contact(
+                name, {"name": name, "email": email, "phone": phone}
+            )
+        elif command == "3":
+            name = input("Enter name to update contact: ")
+            contact = contact_book.search_contact(name)
+            print(contact)
+        elif command == "4":
+            name = input("Enter name to update contact: ")
+            contact_book.delete_contact(name)
+        elif command == "5":
+            contact_book.view_contacts()
+        elif command == "6":
+            print("Contact book closed")
+            break
+
+
+if __name__ == "__main__":
+    main()
